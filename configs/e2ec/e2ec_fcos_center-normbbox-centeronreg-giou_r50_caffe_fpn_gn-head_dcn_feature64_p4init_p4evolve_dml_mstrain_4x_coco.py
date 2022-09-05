c6b62165e2df2cda3a133554dd22f6030a9987f3
zhang tao <zhang_tao@whu.edu.cn>
2022-09-05 14:38:27 +0800

    add 4*mstrain with p2 and dml

---
+++ configs/e2ec/e2ec_fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_feature64_p4init_p4evolve_dml_mstrain_4x_coco.py
@@ -0,0 +1,83 @@
+_base_ = 'e2ec_fcos_r50_caffe_fpn_gn-head_mstrain_4x_coco.py'
+
+model = dict(
+    backbone=dict(
+        dcn=dict(type='DCNv2', deform_groups=1, fallback_on_stride=False),
+        stage_with_dcn=(False, True, True, True),
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint='open-mmlab://detectron2/resnet50_caffe')),
+    neck=dict(
+        type='FPN',
+        out_channels=64,
+        start_level=0,
+        add_extra_convs='on_output',  # use P5
+        num_outs=6,
+        relu_before_extra_convs=True),
+    bbox_head=dict(
+        in_channels=64,
+        norm_on_bbox=True,
+        centerness_on_reg=True,
+        dcn_on_last_conv=True,
+        center_sampling=True,
+        conv_bias=True,
+        loss_bbox=dict(type='GIoULoss', loss_weight=1.0)),
+    contour_proposal_head=dict(
+        in_channel=64),
+    contour_evolve_head=dict(
+        in_channel=64,
+        loss_last_evolve=dict(
+            type='DMLoss',
+            loss_weight=0.33,
+            offsets_stride=4.,
+            key_item_weight=0.5,
+            crit_type='smoothL1'),
+    ),
+    detector_fpn_start_level=1, #start fron P3
+    contour_fpn_start_level=0, # start from P2
+    # training and testing settings
+    test_cfg=dict(nms=dict(type='nms', iou_threshold=0.6)))
+
+# dataset settings
+img_norm_cfg = dict(
+    mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True, poly2mask=False),
+    dict(
+        type='Resize',
+        img_scale=[(736, 256), (736, 608)],
+        multiscale_mode='range',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='AlignSampleBoundary', point_nums=128, reset_bbox=True),
+    dict(type='ContourDefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels',
+         'gt_masks', 'gt_polys', 'key_points_masks', 'key_points']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(736, 512),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=8,
+    train=dict(pipeline=train_pipeline),
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline))
+optimizer_config = dict(_delete_=True, grad_clip=None)
+
+lr_config = dict(warmup='linear')
