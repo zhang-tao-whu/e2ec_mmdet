cf895513ca0ad33fdd4c5b85a2031cc3600060c9
zhang <zhang_tao@whu.edu.cn>
2022-09-25 11:28:46 +0800

    add whu dataset

--- configs/e2ec/e2ec_whu.py
+++ configs/e2ec/e2ec_whu.py
@@ -108,7 +108,7 @@ optimizer = dict(
         custom_keys={'backbone': dict(lr_mult=1.0, decay_mult=1.0)}))
 optimizer_config = dict(_delete_=True, grad_clip=dict(max_norm=0.1, norm_type=2))
 # learning policy
-lr_config = dict(policy='step', step=[45])
+lr_config = dict(_delete_=True, policy='step', step=[45])
 runner = dict(type='EpochBasedRunner', max_epochs=80)
 evaluation = dict(metric=['bbox', 'segm'], interval=15)
 checkpoint_config = dict(interval=5)
